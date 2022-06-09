fe6975217d1555ca3ac5e81653df10ecd8be2d03
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-06-09 17:05:41 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(10+20+"Hello, World!");
+        //30Hello, World!
+        System.out.println("Hello, World!"+10+20);
+        //Hello, World!1020
     }
 }
