6214b456c19c32f0d6ec18be60b2b90ebb2d0ba9
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-11-22 22:03:10 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println(10+20+"Hello, World!");
-        //30Hello, World!
-        System.out.println("Hello, World!"+10+20);
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println("Num is : "+num);
         //Hello, World!1020
     }
 }
